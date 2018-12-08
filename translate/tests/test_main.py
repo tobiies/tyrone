5caacedc1a39c2f2459d939cd8eb2f5e46a1cd54
tobi <32202985+tobiies@users.noreply.github.com>
2018-12-08 21:13:07 +0000

    Add files via upload

---
+++ translate/tests/test_main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from __future__ import unicode_literals
+try:
+    from unittest import mock
+except Exception:
+    import mock
+
+from translate.main import main
+
+from .vcr_conf import vcr
+
+response_template = (
+    '\nTranslation: {}\n'
+    '-------------------------\n'
+    'Translated by: MyMemory\n'
+)
+
+
+@vcr.use_cassette
+def test_main_language_to_translate_required(cli_runner):
+    result = cli_runner.invoke(main, ['hello', 'world'], input='zh')
+    response = response_template.format('你好，世界')
+    assert 'Translate to []: zh\n{}'.format(response) == result.output
+
+
+@vcr.use_cassette
+def test_main_to_language(cli_runner):
+    result = cli_runner.invoke(main, ['-t', 'zh-TW', 'love'])
+    assert response_template.format('爱') == result.output
+
+
+@vcr.use_cassette
+def test_main_to_language_output_only(cli_runner):
+    result = cli_runner.invoke(main, ['-t', 'zh-TW', '-o', 'love'])
+    assert '爱\n' == result.output
+
+
+@vcr.use_cassette
+def test_main_from_language(cli_runner):
+    result = cli_runner.invoke(main, ['--from', 'ja', '--to', 'zh', '美'])
+    assert response_template.format('美') == result.output
+
+
+@mock.patch('translate.main.__version__', '0.0.0')
+def test_main_get_vesion(cli_runner):
+    result = cli_runner.invoke(main, ['--version'])
+    assert 'translate, version 0.0.0\n' == result.output
---
+++ translate/tests/test_provider.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# encoding: utf-8
+try:
+    from unittest import mock
+except Exception:
+    import mock
+
+from translate.providers import MyMemoryProvider, MicrosoftProvider
+
+
+def test_provider_mymemory_languages_attribute():
+    from_lang = 'zh'
+    to_lang = 'en'
+    provider = MyMemoryProvider(to_lang=to_lang, from_lang=from_lang, headers={})
+    expected = '{}|{}'.format(from_lang, to_lang)
+    assert provider.languages == expected
+
+
+def test_provider_mymemory_default_email():
+    provider = MyMemoryProvider(to_lang='en', headers={})
+    assert provider.email == ''
+
+
+@mock.patch('requests.get')
+def test_provider_mymemory_make_request_with_valid_email(mock_requests):
+    mock_requests.return_value.json.return_value = {}
+    valid_email = 'test@valid-email.com'
+    provider = MyMemoryProvider(to_lang='en', headers={}, email=valid_email)
+    provider._make_request('test') == {}
+    assert mock_requests.call_args[1]['params']['de'] == valid_email == provider.email
+
+
+@mock.patch('requests.post')
+@mock.patch('requests.get')
+def test_provider_microsoft_make_request(mock_requests_get, mock_requests_post):
+    class MockRequests:
+        content = b'token'
+
+        def raise_for_status(self):
+            return False
+
+    mock_requests_get.return_value.text.return_value = ''
+    mock_requests_post.return_value = MockRequests()
+    provider = MicrosoftProvider(to_lang='en', headers={}, secret_access_key='secret')
+    provider._make_request('test')
+    assert mock_requests_get.called
+    assert mock_requests_post.called
