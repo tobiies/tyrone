060f653539809ee855ae0916edf65e68ae23f3f5
tobi <32202985+tobiies@users.noreply.github.com>
2018-12-08 13:05:33 +0000

    updated tyrone
    * added some sounds & music
    * changes instances of "comment(s)" to "command(s)"
    * ability to play sounds & music
    * tyrone can now tell riddles
    +
    * message that appears after the use of the "clear" function has been changed to "Screen successfully cleared!"

--- Tyrone.py
+++ Tyrone.py
@@ -6,23 +6,25 @@ from random import randint
 import webbrowser
 import datetime
 import os
+import winsound
 
 action = ["Type in what you want to do: ","Type in the desired action: ","Type something you wanna do: ","Let's do something: ","Do you wanna do something? ","What do you wanna do?: ","Choose something to do: ","Type something you wanna do: ", "Wanna do anything?: ","Wanna do something?: ","Ready to do something?: ","Let's go!: "]
 roasts = ["You're so stupid, I could count your brain cells on one hand.","You're so stupid, you went to the dentist to get Bluetooth.","As a failure, you are a great success.","Come back and talk to me when your I.Q. exceeds your age.","You're the reason this country has to put directions on shampoo.","If you really spoke your mind, you'd be speechless.","Have you been shopping lately? They're selling lives, you should go get one.","You're like Monday mornings, nobody likes you.","How old are you? - Wait I shouldn't ask, you can't count that high.","The last time I saw something like you, I flushed it.","Some babies were dropped on their heads but you were clearly thrown at a wall.","Calling you an idiot would be an insult to all the stupid people.","You, sir, are an oxygen thief!","I can explain something to you, but I can’t understand it for you.","If I wanted to kill myself I'd climb your ego and jump to your IQ.","You're so ugly, when your mom dropped you off at school she got a fine for littering.","You're so ugly, you scared the crap out of the toilet","You're so dumb you got hit by a parked car","You're an idiot","Paigon.","You're a wasteman.","I'd slap you, but that would be animal abuse.","Please shut your mouth when you’re talking to me.","If I had a face like yours, I'd sue my parents.","It's better to let someone think you are an idiot than to open your mouth and prove it."]
 intro = ["So who are you? ","So what's your name? ","What's your name? ","You got a name or something? ","You got a name? "]
 address = ["What's up ","Hi ","Hey ","Yo ","Alright ","Alright then ","Ok ","Come on ","Come on then ","Ok then "]
-facts = ["Sweden has a ski-through McDonald's","Research has shown that dogs actually like the silly, high-pitched voice their owners use to talk to them.","A report found that the free weights at the gym have 362 times more bacteria than a toilet seat.","People who think their opinions are superior to others may be most prone to overestimating their relevant knowledge and ignoring chances to learn more, a study found.","Netflix is responsible for 15% of global Internet traffic.","The 57 on Heinz ketchup bottles represents the number of varieties of pickles the company once had.","The plastic things on the end of shoelaces are called aglets.","You burn more calories sleeping than you do watching TV.","Stressed is Desserts spelled backwards.","Karoke means 'empty orchestra' in Japanese.","Cats sleep 16 to 18 hours per day.","'Rhythm' is the longest English word without a vowel.","Apparently banging your head against a wall burns 150 calories a hour.","Camels have three eyelids to protect themselves from blowing sand.","The word 'queue' is the only word in the English language that is still pronounced the same way when the last four letters are removed.","'Almost' is the longest word in the English language with all the letters in alphabetical order.","Donald Duck comics were banned from Finland because he doesn't wear any pants.","Cap’n Crunch’s full name is Horatio Magellan Crunch.","More than 50% of the people in the world have never made or received a telephone call.","Coconuts kill more people in the world than sharks do. Approximately 150 people are killed each year by coconuts.","Barbie's full name is Barbara Millicent Roberts.","A sneeze can travel as fast as 100 miles per hour."]
-invalid = ["Command is unavailable.","This action isn't available.","This command doesn't appear to work.","I think you've made a mistake somewhere.","Sorry, you might have made a spelling mistake.","Check for any spelling mistakes.","Check for any mistakes.","You might have made a mistake.","Sorry, that's an invalid command.","Invalid comment.","That's an invalid comment.","This command doesn't work."]
+facts = ["Did you know Sweden has a ski-through McDonald's?","Research has shown that dogs actually like the silly, high-pitched voice their owners use to talk to them.","A report found that the free weights at the gym have 362 times more bacteria than a toilet seat.","People who think their opinions are superior to others may be most prone to overestimating their relevant knowledge and ignoring chances to learn more, a study found.","Netflix is responsible for 15% of global Internet traffic.","The 57 on Heinz ketchup bottles represents the number of varieties of pickles the company once had.","The plastic things on the end of shoelaces are called aglets.","You burn more calories sleeping than you do watching TV.","Stressed is Desserts spelled backwards.","Karoke means 'empty orchestra' in Japanese.","Cats sleep 16 to 18 hours per day.","'Rhythm' is the longest English word without a vowel.","Apparently banging your head against a wall burns 150 calories a hour.","Camels have three eyelids to protect themselves from blowing sand.","The word 'queue' is the only word in the English language that is still pronounced the same way when the last four letters are removed.","'Almost' is the longest word in the English language with all the letters in alphabetical order.","Donald Duck comics were banned from Finland because he doesn't wear any pants.","Cap’n Crunch’s full name is Horatio Magellan Crunch.","More than 50% of the people in the world have never made or received a telephone call.","Coconuts kill more people in the world than sharks do. Approximately 150 people are killed each year by coconuts.","Barbie's full name is Barbara Millicent Roberts.","A sneeze can travel as fast as 100 miles per hour."]
+welcome = ["No worries!","No problem.","Don't mention it :)","My pleasure :)","What can I say, except...","You're welcome?","Varsågod!","You're welcome?","It's a pleasure!","It's all good :)","No, thank *you*","Alright.","Ok.","Thank you for the appreciation :)"]
+invalid = ["Command is unavailable.","This action isn't available.","This command doesn't appear to work.","I think you've made a mistake somewhere.","Sorry, you might have made a spelling mistake.","Check for any spelling mistakes.","Check for any mistakes.","You might have made a mistake.","Sorry, that's an invalid command.","Invalid command.","That's an invalid command.","This command doesn't work."]
 bye = ["In a bit.","Until next time...","I'll see you later.","See ya!","Bye.","See ya later!","I bid you adieu!"]
+
+winsound.PlaySound('xp.wav', winsound.SND_FILENAME)
 
 print("What's up? I'm Tyrone.")
 name = input(random.choice(intro))
 
-names = name
-
 time.sleep(1)
 
-print(random.choice(address),names.capitalize(),", let's do some stuff.\n", sep ='')
+print(random.choice(address),name.capitalize(),", let's do some stuff.\n", sep ='')
 time.sleep(1)
 print("But before we do anything, I should let you know that you can get help by typing 'help'.\n")
 
@@ -34,18 +36,46 @@ def command():
 
     if commands == "help":
         print()
-        print("Here are some actions / commands that might help you\n>>> Help (alt = help me)\n>>> Roast (alt = roast me)\n>>> Thanks\n>>> Fact\n>>> Programmer (Tobi's Github)\n>>> About\n>>> Date (date and time)\n>>> Clear (clear screen)\n>>> Github (alt = git)\n>>> Credits (alt = credit)\n>>> Bye\n")
+        print("Here are some actions / commands that might help you\n>>> Help (alt = help me)\n>>> Riddle\n>>> Roast (alt = roast me)\n>>> Thanks\n>>> Fact\n>>> Programmer (Creator's Github)\n>>> About\n>>> Date (date and time)\n>>> Clear (clear screen)\n>>> Github (alt = git)\n>>> Credits (alt = credit)\n>>> Bye\n")
         command()
 
     if commands == "clear":
         cls()
-        print("Successfully cleared!\n")
+        print("Screen successfully cleared!\n")
         command()
 
     if commands == "programmer":
         print("Opening Tobi's Github...")
         webbrowser.open('https://github.com/tobiies', new=0, autoraise=True)
         print()
+        command()
+
+    if commands == "riddle":
+        num = randint(1,3)
+        if num == 1:
+            print("The more you take, the more you leave behind. What am I?\n")
+            y = input("Type 'done' once you're done: ")
+            if y == "done":
+                print("Footsteps.\n")
+            else:
+                print("Riddle cancelled.\nReturning to commands...\n")
+                command()
+        if num == 2:
+            print("What comes once in a minute, twice in a moment, but never in a thousand years?\n")
+            x = input("Type 'done' once you're done: ")
+            if x == "done":
+                print("The letter 'm'.\n")
+            else:
+                print("Riddle cancelled.\nReturning to commands...\n")
+                command()
+        if num == 3:
+            print("A doctor and a bus driver are both in love with the same woman, an attractive girl named Sarah. The bus driver had to go on a long bus trip that would last a week. Before he left, he gave Sarah seven apples. Why?\n")
+            z = input("Type 'done' once you're done: ")
+            if z == "done":
+                print("An apple a day keeps the doctor away!\n")
+            else:
+                print("Riddle cancelled.\nReturning to commands...\n")
+                command()
         command()
 
     if commands == "about":
@@ -74,10 +104,20 @@ def command():
         print("Alright.\n")
         command()
     
-    if commands == "thanks":
-        print("You're welcome?\n")
+    if commands == "thanks": # 1/4 chance of music playing
+        choice = randint(1,4)
+        if choice == 1:
+            print(random.choice(welcome))
+        if choice == 2:
+            print(random.choice(welcome))
+        if choice == 3:
+            print(random.choice(welcome))
+        if choice == 4:
+            print("Playing 'You're Welcome' by Dwayne Johnson...")
+            winsound.PlaySound('moana.wav', winsound.SND_FILENAME)
+        print()
         command()
-        
+
     if commands == "date":
         today = time.strftime('%d/%m/%Y %H:%M:%S')
         print("The current date and time is",today)
@@ -86,7 +126,7 @@ def command():
 
     if commands == "help me":
         print()
-        print("Here are some actions / commands that might help you\n>>> Help (alt = help me)\n>>> Roast (alt = roast me)\n>>> Fact\n>>> Date (date and time)\n>>> Github (alt = git)\n>>> Credits (alt = credit)\n>>> Bye\n")
+        print("Here are some actions / commands that might help you\n>>> Help (alt = help me)\n>>> Riddle \n>>> Roast (alt = roast me)\n>>> Fact\n>>> Date (date and time)\n>>> Github (alt = git)\n>>> Credits (alt = credit)\n>>> Bye\n")
         command()
     
     if commands == "roast":
@@ -123,16 +163,16 @@ def command():
         command()
 
     if commands == "credits":
-        print(">>> Tobi - programmer (github.com/tobiies)\n>>> Pun.me - roasts (pun.me/pages/funny-insults.php)\n>>> Factslides - facts (factslides.com)\n")
+        print(">>> Tobi - programmer (github.com/tobiies)\n>>> Pun.me - roasts (pun.me/pages/funny-insults.php)\n>>> Factslides - facts (factslides.com)\n>>> Riddles.com - riddles (https://www.riddles.com/best-riddles)\n")
         command()
 
     if commands == "credit":
-        print(">>> Tobi - programmer (github.com/tobiies)\n>>> Pun.me - roasts (pun.me/pages/funny-insults.php)\n>>> Factslides - facts (factslides.com)\n")
+        print(">>> Tobi - programmer (github.com/tobiies)\n>>> Pun.me - roasts (pun.me/pages/funny-insults.php)\n>>> Factslides - facts (factslides.com)\n>>> Riddles.com - riddles (https://www.riddles.com/best-riddles)\n")
         command()
 
     if commands == "bye":
         print(random.choice(bye))
-        time.sleep(randint(2,4))
+        winsound.PlaySound('xp-shutdown.wav', winsound.SND_FILENAME)
         exit()
 
     else:
